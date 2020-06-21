57f053b0b85ebc24f6b892f425728d148a01b85b
hiruruku <noboru_nishizawa@nifty.com>
2020-06-21 22:27:58 +0900

    ALSL#54_3 ArticleControllerから受けた$articles配列をloopして表示

--- laravel/resources/views/articles/index.blade.php
+++ laravel/resources/views/articles/index.blade.php
@@ -1,33 +1,38 @@
-@extends('app') <!-- app.blade.phpの継承 -->
+@extends('app')
+<!-- app.blade.phpの継承 -->
 
-@section('title', '記事一覧') <!-- titleセクションに、第2引数を埋め込む -->
+@section('title', '記事一覧')
+<!-- titleセクションに、第2引数を埋め込む -->
 
-@section('content') <!-- contentセクション　-->
+@section('content')
+<!-- contentセクション　-->
 <!-- 固定幅 -->
-  <div class="container">　
+<div class="container">　
+   @foreach($articles as $article) {{-- controllerから渡された$articles配列をloop処理 --}}
     <!-- cardクラス(card-body3つ) mt-3(spacing)-->
     <div class="card mt-3">
-      <!-- d-flex >> display:flex flex-row >> flex-direction:row -->
-      <div class="card-body d-flex flex-row">
-        <!-- mdbootstrap(Font Awesomeでもできる -->
-        <i class="fas fa-user-circle fa-3x mr-1"></i>
-        <div>
-          <div class="font-weight-bold">
-            ユーザー名
-          </div>
-          <div class="font-weight-lighter">
-            2020/2/1 12:00
-          </div>
+        <!-- d-flex >> display:flex flex-row >> flex-direction:row -->
+        <div class="card-body d-flex flex-row">
+            <!-- mdbootstrap(Font Awesomeでもできる -->
+            <i class="fas fa-user-circle fa-3x mr-1"></i>
+            <div>
+                <div class="font-weight-bold">
+                   {{ $article->user->name }}
+                </div>
+                <div class="font-weight-lighter">
+                {{ $article->created_at->format('Y/m/d H:i') }} {{-- {{}}の中はXSS攻撃防御でescapeされている --}}
+                </div>
+            </div>
         </div>
-      </div>
-      <div class="card-body pt-0 pb-2">
-        <h3 class="h4 card-title">
-          記事タイトル
-        </h3>
-        <div class="card-text">
-          記事本文
+        <div class="card-body pt-0 pb-2">
+            <h3 class="h4 card-title">
+                {{ $article->title }}
+            </h3>
+            <div class="card-text">
+                {{!! nl2br(e($article->body)) !!}} {{-- e()でescape、改行コードをbrに変換し!!でエスケープ解除 --}}
+            </div>
         </div>
-      </div>
     </div>
-  </div>
+    @endforeach
+</div>
 @endsection
